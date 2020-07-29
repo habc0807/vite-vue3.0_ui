f9fd2a4f4897a999c186f4f29bca5e01fe9a77f3
habc0807 <gao0807@foxmail.com>
2020-07-29 14:01:05 +0800

    feat: finished

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-// import Frank from './components/Frank.vue'
-// import Frank2 from './components/Frank2.vue'
-import './index.scss'
-import { createWebHashHistory, createRouter } from 'vue-router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.scss";
+import { router } from './router';
 
-const history = createWebHashHistory()
-const router = createRouter({
-  history: history,
-  routes: [
-    {
-      path: '/',
-      component: Home
-    },
-    {
-      path: '/doc',
-      component: Doc
-    }
-  ]
-})
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,21 @@
 <template>
-    <div>
-        <Topnav />
-        <div class="banner">
-            <h1>咕噜UI</h1>
-            <h2>一个厉害的UI框架</h2>
-            <p>
-                <a href="">github</a>
-                <a href="">开始</a>
-            </p>
-        </div>
+  <div>
+    <Topnav />
+    <div class="banner">
+      <h1>轱辘UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
+      <p class="actions">
+        <a href="https://github.com/habc0807">GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
     </div>
+  </div>
 </template>
-<script lang='ts'>
+<script lang="ts">
 import Topnav from "../components/Topnav.vue";
 export default {
-    components: { Topnav },
-}
+  components: { Topnav },
+};
 </script>
 <style lang="scss" scoped>
 .banner {
